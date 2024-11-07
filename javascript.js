6a51e7a2277164f3a29620e7165af32cc9505e80
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-11-07 12:40:03 +0000

    Add more calculator functionality
    Operators can be used on the previously displayed answer, they can also
    be chained together without hitting equals. Any calculation errors are
    displayed as such. Decimals work and round nicely.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 let num1 = "";
 let num2 = "";
 let op = "";
+let chain = false;
 
 let numbers = document.querySelectorAll(".number");
 let operators = document.querySelectorAll(".operator");
@@ -10,31 +11,47 @@ let clear = document.querySelector("#clear");
 
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
+        if(display.value == "Error") {
+            return;
+        }
+
         if(op == "") {
-            // if operator is blank, add the digit into the display space for num1
-            num1 += number.textContent;
-            display.value = num1;
+            // chain is set when the calc hasn't been cleared from the previous operation
+            // if numbers are typed after the answer is displayed, set num1 and set chain false
+            if(chain) {
+                num1 = number.textContent;
+                display.value = num1;
+                chain = false;
+            }
+            else {
+                // if operator is blank, add the digit into the display space for num1
+                num1 += number.textContent;
+                display.value = num1;
+            }
+            
         }
         else {
             // if the operator is present, add the digit to num2
             num2 += number.textContent;
             display.value = num2;
         }
+        
     });
 });
 
 operators.forEach((operator) => {
     operator.addEventListener("click", () => {
+        // if the user hits the next operator before equals then perform the calculation
+        if(op != "" && num2 != "") {
+            equate();
+        }
+
         op = operator.textContent;
     });
 });
 
 equals.addEventListener("click", () => {
-    let ans = operate(op, parseInt(num1),  parseInt(num2)); // will not handle decmials, will need to fix
-    display.value = ans;
-    num1 = "";
-    num2 = "";
-    op = "";
+    equate();
 });
 
 clear.addEventListener("click", () => {
@@ -42,6 +59,7 @@ clear.addEventListener("click", () => {
     num1 = "";
     num2 = "";
     op = "";
+    chain = false;
 });
 
 function add(num1, num2) {
@@ -61,6 +79,11 @@ function multiply(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
+    
+    if(operator == "" && !isNaN(num1)){
+        return num1;
+    }
+
     switch(operator) {
         case "+":
             return add(num1, num2);
@@ -70,5 +93,22 @@ function operate(operator, num1, num2) {
             return divide(num1, num2);
         case "x": 
             return multiply(num1, num2);
+    }
+}
+
+function equate() {
+    let ans = operate(op, parseFloat(num1),  parseFloat(num2));
+
+    if(isNaN(ans)) {
+        display.value = "Error";
+    }
+    else {
+        ans = +ans.toFixed(10); //round to 10 decimals, + removes trailing 0s
+        ans = ans.toString().slice(0, 11); // limit entire number to 11 characters to fit display
+        display.value = ans;
+        num1 = ans;
+        num2 = "";
+        op = "";
+        chain = true;
     }
 }
