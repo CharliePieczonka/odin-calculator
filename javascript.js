1901c40216ec121f9de5f3aa3ad95dfdbae346e1
Charlie Pieczonka <49101768+CharliePieczonka@users.noreply.github.com>
2024-11-07 11:49:02 +0000

    Add basic functionality to UI
    UI works and calculations can be made when using the equals button.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,49 @@
+let num1 = "";
+let num2 = "";
+let op = "";
+
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let equals = document.querySelector("#equals");
+let display = document.querySelector("#display");
+let clear = document.querySelector("#clear");
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if(op == "") {
+            // if operator is blank, add the digit into the display space for num1
+            num1 += number.textContent;
+            display.value = num1;
+        }
+        else {
+            // if the operator is present, add the digit to num2
+            num2 += number.textContent;
+            display.value = num2;
+        }
+    });
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        op = operator.textContent;
+    });
+});
+
+equals.addEventListener("click", () => {
+    let ans = operate(op, parseInt(num1),  parseInt(num2)); // will not handle decmials, will need to fix
+    display.value = ans;
+    num1 = "";
+    num2 = "";
+    op = "";
+});
+
+clear.addEventListener("click", () => {
+    display.value = "";
+    num1 = "";
+    num2 = "";
+    op = "";
+});
+
 function add(num1, num2) {
     return num1 + num2;
 }
